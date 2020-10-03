9b22dd52976d0a17b32bae4144cddc899b495487
Joseph dominic <josephdominicmathew@gmail.com>
2020-10-03 16:31:44 +0530

    added check prime or not

---
+++ Language/Python/check prime or not.py
@@ -0,0 +1,14 @@
+number = int(input("Enter any number: "))
+#checking prime or not
+if number > 1:
+    for i in range(2, number):
+        if (number % i) == 0:
+            print(number, "is not a prime number")
+            break
+    else:
+        print(number, "is a prime number")
+
+# if the entered number is less than or equal to 1
+# then it is not prime number
+else:
+    print(number, "is not a prime number")
